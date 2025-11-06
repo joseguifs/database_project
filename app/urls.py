06bbdad7a4a4eafd7a2b9c3f8ac216c6be85f868
joseguifs <joseguilhermeagente007@gmail.com>
2025-11-05 22:36:01 -0300

    testem essa view bem daora

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from fornecedor.views import teste
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('teste/', teste, name = 'teste'),
 ]
--- fornecedor/views.py
+++ fornecedor/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
+
 
 # Create your views here.
+def teste(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM FORNECEDORES LIMIT 0;")
+        colunas = [desc[0] + " " for desc in cursor.description]
+        return HttpResponse(colunas)
